67fbac6f65e146fc8d302d6019024af24b05db9f
michaelgichia <mqyynm@gmail.com>
2016-06-08 20:13:12 +0300

    initialize database functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 from flask_moment import Moment 
 from flask_bootstrap import Bootstrap
 from config import config 
 
 bootstrap = Bootstrap()
 moment = Moment()
+db = SQLAlchemy()
 
 def create_app(config_name):
 	app = Flask(__name__)
@@ -13,6 +15,8 @@ def create_app(config_name):
 
 	bootstrap.init_app(app)
 	moment.init_app(app)
+	db.init_app(app)
+	
 
 	from main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
