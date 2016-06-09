fa14138f00de2d40e839808b4a0255dc510de094
michaelgichia <mqyynm@gmail.com>
2016-06-09 05:41:27 +0300

    Token generation and verification functionality

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from . import db
 from flask_login import UserMixin
@@ -21,6 +22,7 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), unique=True, index=True)
 	password_hash = db.Column(db.String(128))
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	confirmed = db.Column(db.Boolean, default=False)
 	
 
 	@property
@@ -37,6 +39,25 @@ class User(UserMixin, db.Model):
 	@login_manager.user_loader
 	def load_user(user_id):
 		return User.query.get(int(user_id))
+
+	def generate_confirmation_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'confirm': self.id})
+
+	def confirm(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+
+		except:
+			return False
+
+		if data.get('confirm') != self.id:
+			return False
+			
+		self.confirmed = True
+		db.session.add(self)
+		return True
 
 	def __repr__(self):
 		return '<User %r>' % self.username
