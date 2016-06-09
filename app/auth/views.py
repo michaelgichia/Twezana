917c0c713aec948cc4fd867b8c5196c5253e37c3
michaelgichia <mqyynm@gmail.com>
2016-06-09 06:03:54 +0300

    added account confirmation routes and confirm token generator

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,8 @@ from flask_login import logout_user, login_required, current_user, login_user
 from . import auth
 from ..models import User 
 from .form import LoginForm, RegistrationForm
-from .. import db 
+from .. import db
+from ..email import send_email 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,6 +36,42 @@ def register():
                     username=form.username.data,
                     password=form.password.data)
         db.session.add(user)
-        flash('You can now login.')
+		db.session.commit()
+		token = user.generate_confirmation_token()
+		send_email(user.email, 'Confirm Your Account',
+		'auth/email/confirm', user=user, token=token)
+		flash('A confirmation email has been sent to this email. Please confirm your account')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	if current_user.confirm(token):
+		flash('You have confirmed your account. Thanks!')
+	else:
+		flash('The confirmation link is invalid or has expired.')
+	return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+	return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous() or current_user.confirmed:
+		return redirect('main.index')
+	return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_email('auth/email/confirm',
+				'Confirm Your Account', user, token=token)
+	flash('A new confirmation email has been sent to you by email.')
+	return redirect(url_for('main.index'))
