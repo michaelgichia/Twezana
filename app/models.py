9786542df2971b252827e782169cc19ff98aba79
michaelgichia <mqyynm@gmail.com>
2016-06-11 05:09:39 +0300

    user roles assignment

--- app/models.py
+++ app/models.py
@@ -51,6 +51,14 @@ class User(UserMixin, db.Model):
 	password_hash = db.Column(db.String(128))
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 	confirmed = db.Column(db.Boolean, default=False)
+
+	def __init__(self, **kwargs):
+		super(User, self).__init__(**kwargs)
+		if self.role is None:
+			if self.email == current_app.config['TWEZANA_ADMIN']:
+				self.role = Role.query.filter_by(permissions=0xff).first()
+			if self.role is None:
+				self.role = Role.query.filter_by(default=True).first()
 	
 
 	@property
