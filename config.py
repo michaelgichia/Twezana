5007710402b6d76d59ef6eac6758310e810c9986
michaelgichia <mqyynm@gmail.com>
2016-06-11 06:21:41 +0300

    smpt configurations update

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'lkkjuy6786764565wsrguijopo090788676tgjhyijkhjohk'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
-	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	MAIL_USERNAME = 'mail@gmail.com' #os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = 'pass'	#os.environ.get('MAIL_PASSWORD')
 	TWEZANA_MAIL_SUBJECT_PREFIX = '[Twezana]'
-	TWEZANA_MAIL_SENDER = 'Twezana Admin gichuru.gichia@gmail.com'
+	TWEZANA_MAIL_SENDER = 'gichuru.gichia@gmail.com'
 	TWEZANA_ADMIN = os.environ.get('TWEZANA_ADMIN')
 
 	@staticmethod
