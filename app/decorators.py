8e5b2fead77c091b5492f5dbb54b0b223c18a5a5
michaelgichia <mqyynm@gmail.com>
2016-06-12 22:00:37 +0300

    updated permissions

--- app/decorators.py
+++ app/decorators.py
@@ -2,15 +2,16 @@ from functools import wraps
 from flask import abort
 from flask_login import current_user
 
+
 def permission_required(permission):
 	def decorator(f):
-	@wraps(f)
+		@wraps(f)
 		def decorated_function(*args, **kwargs):
 			if not current_user.can(permission):
 				abort(403)
 			return f(*args, **kwargs)
 		return decorated_function
-return decorator
+	return decorator
 
 def admin_required(f):
 	return permission_required(Permission.ADMINISTER)(f)
