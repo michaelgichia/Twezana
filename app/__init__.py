30cf6277864589658421325563d26869e09e20da
michaelgichia <mqyynm@gmail.com>
2016-06-13 00:47:44 +0300

    restructuring confirmation email.

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
-	app = Flask(__name__)
+	app = Flask(__name__, static_url_path="/static", static_folder="static")
 	app.config.from_object(config[config_name])
 	config[config_name].init_app(app)
 
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,8 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
+# @main.app_context_processor
+# def inject_permissions():
+# 	return dict(Permission=Permission)
+
 from . import views, errors
