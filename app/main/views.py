8023ff8180f2ec647d44a75e16ed4bbc914b1a1f
michaelgichia <mqyynm@gmail.com>
2016-06-13 04:33:22 +0300

    add a user profile route

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, abort 
 from . import main
 from flask_login import current_user
+from ..models import User 
 
 @main.route('/')
 def index():
 	return render_template('index.html', current_user=current_user) 
+
+@main.route('/user/<username>')
+def user(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		abort(404)
+	return render_template('user.html', user=user)
