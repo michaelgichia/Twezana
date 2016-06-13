52fec531b1b59cfaa16d59e3e9b1b1e715c776de
michaelgichia <mqyynm@gmail.com>
2016-06-13 04:26:03 +0300

    initializing last seen

--- app/models.py
+++ app/models.py
@@ -108,6 +108,10 @@ class User(UserMixin, db.Model):
 	def is_administrator(self):
 		return self.can(Permission.ADMINISTER)
 
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		db.session.add(self)
+
 	def __repr__(self):
 		return '<User %r>' % self.username
 
